efc9ce24737e521dc8d78c28f0d0273e7f9e6bcb
Olle Jonsson <olle.jonsson@gmail.com>
2016-10-21 13:24:35 +0200

    Spec: Rake integration naming

--- spec/opbeat/integration/rake_spec.rb
+++ spec/opbeat/integration/rake_spec.rb
@@ -9,13 +9,13 @@ end
 # :nocov:
 
 if defined?(Rake)
-  RSpec.describe 'a failing Rake task', start_without_worker: true do
+  RSpec.describe 'Rake integration', start_without_worker: true do
     before :context do
       Rake.application = Rake::Application.new
       Rake.application.rake_require('rakefile', [File.join(File.dirname(__FILE__), 'rake')])
     end
 
-    it 'captures and reports exceptions to opbeat' do
+    it 'captures and reports exceptions to Opbeat' do
       expect do
         Rake.application.invoke_task('raise_error[12,11]')
       end.to raise_error(Exception).with_message('exception')
@@ -23,7 +23,7 @@ if defined?(Rake)
       expect(Opbeat::Client.inst.queue.length).to be 1
     end
 
-    it 'captures and reports exceptions in dependencies to opbeat' do
+    it 'captures and reports exceptions in dependent tasks to Opbeat' do
       expect do
         Rake.application.invoke_task('depends_on_raise_error[12,11]')
       end.to raise_error(Exception).with_message('exception2')
